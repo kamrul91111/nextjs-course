882aafb51946b3d868a709461978c54cbb6ff426
kamrul91111 <kamrul.islam91111@gmail.com>
2022-02-10 16:31:06 +1100

    next js project

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+         This is the Next JS Course
         </h1>
 
         <p className={styles.description}>
@@ -67,3 +67,6 @@ export default function Home() {
     </div>
   )
 }
+
+
+export default Home;
